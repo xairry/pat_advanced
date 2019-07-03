c16978656a350e2e106ab71eeaecf091b3667be8
unknown <xairry7@gmail.com>
2019-07-03 09:40:26 +0800

    :sunglasses:

--- X_1153_ DecodeRegistrationCardofPAT_25.cpp
+++ X_1153_ DecodeRegistrationCardofPAT_25.cpp
@@ -1,20 +1,55 @@
 #include<stdio.h>
 #include<string>
 #include<vector>
+#include<iostream>
+#include<unordered_map>
+#include<algorithm>
 using namespace std;
-
+struct node {
+	string t;
+	int val;
+};
+inline bool cmp (const node &a,const node &b) {
+	return a.val!=b.val?a.val>b.val:a.t<b.t;
+}
 int main() {
-	int N,M;
-	scanf("%d %d",&N,&M);
-	vector<string> ori;ori.clear();
-	while(N--) {
-		string str;
-		scanf("%s",&str);
-		ori.push_back(str);
-		
+	int n,k,num;
+	string s;
+	cin>>n>>k;
+	vector<node> ori(n);
+	for(int i=0;i<n;++i) {
+		cin>>ori[i].t>>ori[i].val;
 	}
-	for(int i=0;i<N;i++) {
-		printf("%c",ori[i]);
+	printf("haha");
+	for(int i=0;i<k;i++) {
+		cin>>num>>s;
+		printf("Case %d: %d %s\n",i+1,num,s.c_str());
+		vector<node> ans;
+		int cnt = 0,sum = 0;
+		if (num == 1) {
+			for(int j=0;j<n;j++) {
+				if(ori[j].t[0] == s[0]) ans.push_back(ori[j]);
+			}
+		}else if ( num == 2) {
+			for(int j=0;j<n;j++) {
+				if(ori[j].t.substr(1,3) == s) {
+					cnt++;
+					sum+=ori[j].val;
+				}
+			}
+			if(cnt!=0)printf("%d %d\n",cnt,sum);
+		}else if (num == 3) {
+			unordered_map<string,int> m;
+			for(int j=0;j<n;j++) {
+				if(ori[j].t.substr(4,6) == s) m[ori[j].t.substr(1,3)]++;
+			}
+			for(auto it:m)ans.push_back({it.first,it.second});
+		}
+		sort(ans.begin(),ans.end(),cmp);
+		for(int j=0;j<ans.size();j++){
+			printf("%s %d\n",ans[j].t.c_str(),ans[j].val);
+		}
+		if(((num==1||num==3) && ans.size()==0)||(num==2 &&cnt == 0))printf("NA\n");
 	}
 	
 	
---
+++ X_1155_HeapPaths_30.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<vector>
+using namespace std;
+int n,isMin = 1,isMax = 1,a[1009];
+vector<int> v;
+void dfs(int index) {
+	if(index*2 >n && index*2+1>n) {
+		if(index <=n) {
+			for(int i=0;i<v.size();i++) {
+				printf("%d%s",v[i],i!=v.size()-1?" ":"\n");
+			}
+		}
+	}else{
+		v.push_back(a[index*2+1]);
+		dfs(index*2+1);
+		v.pop_back();
+		v.push_back(a[index*2]);
+		dfs(index*2);
+		v.pop_back();
+	}
+}
+int main() {
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		scanf("%d",&a[i]);
+	}
+	v.push_back(a[1]);
+	dfs(1);
+	for(int i=2;i<=n;i++) {
+		if(a[i/2]>a[i])isMin = 0;
+		if(a[i/2]<a[i])isMax = 0;
+	}
+	if (isMin == 1){
+		printf("Min Heap");
+	}else{
+		printf("%s",isMax==1?"Max Heap":"Not Heap");
+		
+	}
+	
+	
+	return 0;
+}
