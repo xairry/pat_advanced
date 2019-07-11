d19fc45cb4d24d452afdf681fd90890142d349c8
xairry <xairry7@gmail.com>
2019-07-11 22:05:54 +0800

    :sunglasses:

---
+++ XYY_1042_ShufflingMachine_20.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+const int N = 54;
+char mp[5] = {'S','H','C','D','J'};
+int start[N+1],end[N+1],next[N+1];
+
+int main() {
+ int K;
+ scanf("%d",&K);
+ for(int i=1;i<=N;i++) {
+   start[i] = i;
+ }
+
+ for(int i=1;i<=N;i++) {
+   scanf("%d",&next[i]);
+ }
+
+ for(int sp = 0;sp<K;sp++) {
+   for(int i=1;i<=N;i++) {
+     end[next[i]] = start[i];
+   }
+   for(int i=1;i<=N;i++) {
+     start[i] = end[i];
+   }
+ }
+ for(int i=1;i<=N;i++) {
+   if(i!=1) printf(" ");
+   start[i]--;
+   printf("%c%d",mp[start[i] / 13],start[i] % 13 + 1);
+ }
+
+
+
+ return 0;
+}
